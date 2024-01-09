e1e4000c6a56243cc8093570be3b09cbe3db85e5
Michael Hudson <mnkotagu@gmail.com>
2024-01-09 19:26:06 +0300

    Improve API logic to hanlde calculation of total score after each user submission

--- src/Controller/API/QuizController.php
+++ src/Controller/API/QuizController.php
@@ -32,13 +32,11 @@ class QuizController extends AbstractController
     /**
      * @throws Exception
      */
-    #[Route('/evaluate-answer', name: 'api_evaluate_answer', options: ['expose' => true], methods: ['GET'])]
+    #[Route('/evaluate-answer', name: 'api_evaluate_answer', options: ['expose' => true], methods: ['POST'])]
     public function evaluateAnswer(GeoQuestHelper $app, Request $request): Response
     {
 
         $data = $request->getContent();
-
-        $data = '{"id":2187,"factType":"Capitals","quizIdentifier":"018cecca-7b46-7ade-96ce-2e06d0a2bf41","answerId":14}';
 
         return new JsonResponse(
             $app->evaluateCurrentAnswer(json_decode($data, true))
